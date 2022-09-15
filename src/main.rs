fcb84796f20c4e94dd46dddc952af695cd2c4282
Sacha924 <24448@student.dorset-college.ie>
2022-09-15 16:58:00 +0200

    add some comments

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     loop{
     println!("Please input your guess");
 
-    let mut guess = String::new();
+    let mut guess = String::new();  //this line has created a mutable variable that is currently bound to a new, empty instance of a String.
 
     io::stdin()
         .read_line(&mut guess)
@@ -42,6 +42,23 @@ fn main() {
 }
 
 /*Commentaries on the method use 
+
+io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+we call the stdin function from the io module, which will allow us to handle user input. If we hadn’t imported the io library with use std::io at the beginning of the program, we could still use the function by writing this function call as std::io::stdin. The stdin function returns an instance of std::io::Stdin, which is a type that represents a handle to the standard input for your terminal.
+
+Next, the line .read_line(&mut guess) calls the read_line method on the standard input handle to get input from the user. We’re also passing &mut guess as the argument to read_line to tell it what string to store the user input in. The full job of read_line is to take whatever the user types into standard input and append that into a string (without overwriting its contents), so we therefore pass that string as an argument. The string argument needs to be mutable so the method can change the string’s content.
+
+The & indicates that this argument is a reference, which gives you a way to let multiple parts of your code access one piece of data without needing to copy that data into memory multiple times. References are a complex feature, and one of Rust’s major advantages is how safe and easy it is to use references. You don’t need to know a lot of those details to finish this program. For now, all you need to know is that like variables, references are immutable by default. Hence, you need to write &mut guess rather than &guess to make it mutable
+
+
+
+
+
+
+
 Explication du : let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
 The trim method on a String instance will eliminate any whitespace at the beginning and end, which we must do to be able to compare the string to the u32, which can only contain numerical data. The user must press enter to satisfy read_line and input their guess, which adds a newline character to the string. For example, if the user types 5 and presses enter, guess looks like this: 5\n. The \n represents “newline”. (On Windows, pressing enter results in a carriage return and a newline, \r\n). The trim method eliminates \n or \r\n, resulting in just 5.
@@ -54,5 +71,8 @@ Because it might fail, the parse method returns a Result type, much as the read_
 
 
 
-
+Explication du Ordering:
+The Ordering type is another enum and has the variants Less, Greater, and Equal. These are the three outcomes that are possible when you compare two values.
+We use this, and also the comp Method. The cmp method compares two values and can be called on anything that can be compared.
+Here it’s comparing the guess to the secret_number. Then it returns a variant of the Ordering enum we brought into scope with the use statement. We use a match expression to decide what to do next based on which variant of Ordering was returned from the call to cmp with the values in guess and secret_number.
 */
